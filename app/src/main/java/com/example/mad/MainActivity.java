ee65af3843f4c3cb70a4be8c7c2d7ae0a3c350f0
hello3456 <howardleehj@gmail.com>
2025-12-10 12:39:57 +0800

    update file

--- app/src/main/java/com/example/mad/MainActivity.java
+++
@@ -1,9 +0,0 @@
-package com.example.mad;
-
-import android.content.Intent;
-import android.os.Bundle;
-import androidx.appcompat.app.AppCompatActivity;
-
-public class MainActivity extends AppCompatActivity {
-
-}
